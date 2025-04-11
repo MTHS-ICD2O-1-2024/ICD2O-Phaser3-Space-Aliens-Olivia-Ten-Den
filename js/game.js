0121d0767bb1b70b2ef63d9efd6ca47a9966ef6a
Olivia-Ten-Den <olivia.ten-den@stu.ocsb.ca>
2025-04-11 16:57:05 +0000

    game js added

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
+/* global Phaser */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: April 11
 // This file contains the JS functions for index.html
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
