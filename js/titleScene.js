a49fddd5a899d7dfc2568c7fa261dae09f10861d
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-04-29 14:43:36 +0000

    fixed format

--- js/titleScene.js
+++ js/titleScene.js
@@ -18,10 +18,10 @@ class TitleScene extends Phaser.Scene {
 
     this.titleSceneBackgroundImage = null
     this.titleSceneText = null
-    this.titleSceneTextStyle = { 
-      font: '200px Times', 
-      fill: '#fde4b9', 
-      align: 'center' 
+    this.titleSceneTextStyle = {
+      font: '200px Times',
+      fill: '#fde4b9',
+      align: 'center'
     }
   }
 
@@ -57,8 +57,8 @@ class TitleScene extends Phaser.Scene {
     this.titleSceneBackgroundImage.y = 1080 / 2
 
     this.titleSceneText = this.add
-    .text(1920 / 2, (1080 / 2) + 350, 'Space Aliens', this.titleSceneTextStyle)
-    .setOrigin(0.5)
+      .text(1920 / 2, (1080 / 2) + 350, 'Space Aliens', this.titleSceneTextStyle)
+      .setOrigin(0.5)
   }
 
   /**
